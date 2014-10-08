b637bf9a1308ae7d3aeebd8bbbf829dec1412f8b
jonschlinkert <dev@sellside.com>
2014-10-07 22:58:37 -0400

    reallow certain keywords

--- cli.js
+++ cli.js
@@ -13,6 +13,16 @@ var pkg = require(localPkg);
 
 log.runner = 'keywords';
 
+function pad(arr) {
+  return ['\n' ].concat(arr) //.map(clean)
+    .reduce(function(acc, key, i) {
+      key = key.split(' ');
+      acc = acc.concat(key);
+      return acc;
+    }, []).sort()
+    .join('\n   ' + log.sep + ' ');
+}
+
 var add = argv.add || argv.a || argv._;
 var remove = argv.remove || argv.r;
 
@@ -27,15 +37,16 @@ if (typeof remove === 'string') {
   remove = remove.split(',');
 }
 
-console.log();
-
-if (add && add.length) {
-  log.inform('adding', add);
-}
-if (remove && remove.length) {
-  log.inform('removing', remove);
-}
 
 pkg.keywords = keywords(add, remove);
 fs.writeFileSync(localPkg, JSON.stringify(pkg, null, 2));
+
+console.log();
+
+if ((add && add.length) || (remove && remove.length)) {
+  log.inform('updated', pad(pkg.keywords));
+}
+
+console.log();
+
 log.success('  Done.');
